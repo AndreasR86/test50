86b37e1eb495c75381ea094b65092418ac33f953
AndreasR86 <AndreasR86@hotmail.com>
2022-10-17 14:56:19 +0200

    first

---
+++ Hello.java
@@ -0,0 +1,40 @@
+import java.util.*;
+
+
+public class Hello {
+
+   public static void main(String[] args)
+   {
+       /*System.out.println("Hello world");
+       var scan =new Scanner(System.in);
+               System.out.println("hur gammal är du");
+       int age= scan.nextInt();
+       System.out.println(age);/
+
+
+
+       var namn= new Scanner(System.in);
+       System.out.println("vad heter du");
+       String name= namn.nextLine();
+       System.out.println(name);
+
+       var scan= new Scanner(System.in);
+       System.out.println("Hur lång är du");
+       double längd = scan.nextDouble();
+       System.out.println(längd);*/
+
+       System.out.println("xxxx");
+       System.out.println("x  x");
+       System.out.println("x  x");
+       System.out.println("xxxx");
+
+
+
+
+
+
+
+
+
+    }
+}
